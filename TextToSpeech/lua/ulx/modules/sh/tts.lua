64374db212bf2254402b83059fcb6e5431901f43
GarrettBrad <GarrettBrad@users.noreply.github.com>
2016-12-22 14:37:35 -0800

    Update tts.lua

--- TextToSpeech/lua/ulx/modules/sh/tts.lua
+++ TextToSpeech/lua/ulx/modules/sh/tts.lua
@@ -1,35 +1,38 @@
-local CATEGORY_NAME = "Chat"
+local CATEGORY_NAME = "Chat" 
 
 if ( SERVER ) then
-	util.AddNetworkString("T2S") 
-end
+	util.AddNetworkString("T2S")
+end 
 
 if ( CLIENT ) then
 	net.Receive("T2S", function(len)
 		local text = ""
 		text = net.ReadString()
 
-		sound.PlayURL("http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=" .. text ..  "&tl=en", "mono", function(chan, num, str)
-			-- body
-		end ) 
-	end ) 
+		sound.PlayURL("http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=" .. text .. "&tl=en", "mono", function(chan, num, str)	end )
+	end )
 end 
 
-function ulx.tts( calling_ply, text )
+function ulx.tts( calling_ply, text )                   
 	if calling_ply and calling_ply:IsValid() then
-		text = string.Trim( text )
+		local str = ""
 
-		if text == "" then return end
-		
-		text = string.gsub( text, ' ', '%20' ) 
+		text = string.Explode(" ", text)
 
-		net.Start("T2S") 
-			net.WriteString(text) 
+		if text == {} or text[1] == "" then return end
+			
+		for k,v in pairs(text) do
+			str = str .. v .. "%20"
+		end
+
+		net.Start("T2S")
+			net.WriteString(str)
 		net.Broadcast()
-	end 
-end 
+	end
+end     
 
 local tts = ulx.command( CATEGORY_NAME, "ulx t2s", ulx.tts, "!t2s", true )
-tts:addParam{ type=ULib.cmds.StringArg, hint="text to say", ULib.cmds.takeRestOfLine } 
-tts:defaultAccess( ULib.ACCESS_SUPERADMIN ) 
-tts:help( "Text 2 speach." ) 
+tts:addParam{ type=ULib.cmds.StringArg, hint="text to say", ULib.cmds.takeRestOfLine }
+tts:defaultAccess( ULib.ACCESS_SUPERADMIN )
+tts:help( "Text 2 speach." )
+
